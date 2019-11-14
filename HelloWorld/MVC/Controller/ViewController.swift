5115b5bf074e3b0f8bcc212376f151f1d88e9141
Srikanth <Gali_Srikanth@msrlimited.com>
2019-11-14 14:51:22 +0530

    This is my first swift project

---
+++ HelloWorld/MVC/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Gali Srikanth on 14/11/19.
+//  Copyright © 2019 Msrit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
